c0c985861fab2a6130831a440f55d635b94dd81e
cfs5403 <cfs5403@gmail.com>
2015-06-14 03:30:17 +0800

    latest999

---
+++ test/Module_B_Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nathan Chai
+ */
+public class Module_B_Test {
+    
+    @Test    
+    public void test() {
+        Module_B module_b = new Module_B();
+        
+        assertEquals("This should return Two", 2, module_b.return_two());
+    }
+    
+}
