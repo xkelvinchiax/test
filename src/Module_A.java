3306f0b49ddc0477767564ea10b2a8a4d4f74eda
cfs5403 <cfs5403@gmail.com>
2015-06-14 01:27:44 +0800

    fuck888

---
+++ src/Module_A.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nathan Chai
+ */
+public class Module_A {
+    
+    public Module_A() {
+        
+    }
+    
+    public int return_num(){
+        return -1;
+    }
+    
+}
