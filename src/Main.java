a4569c355e338463cd3c0478533da3fe2589ab1f
cfs5403 <cfs5403@gmail.com>
2015-06-14 00:58:01 +0800

    fuck2

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello world");
-        asdasdasdas
-                asdasdasda
+asdasdas
+        dasd
+        asdasd
     }
 }
